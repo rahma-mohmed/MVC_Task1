ef5bd4a224267bb4be311a07530dc83e87ca7342
rahma-mohmed <engrahma1422003@gmail.com>
2024-03-11 03:53:34 +0200

    Task_1

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1_Day1.Models;
+
+namespace Task1_Day1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student student = StudentList.Students.FirstOrDefault(x => x.Id == id);
+            return View("getDetails" , student);
+        }
+
+        public IActionResult GetAll()
+        {
+            List<Student> li = StudentList.Students;
+            return View("All",li);
+        }
+    }
+}
---
+++ Models/StudentList.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Task1_Day1.Models
+{
+    public class StudentList
+    {
+        public static List<Student> Students { get; set; }
+
+        static StudentList()
+        {
+            Students = new List<Student>();
+            Students.Add(new Student() { Id = 1 , Name = "Rahma" , Address= "Damietta" ,Image = "1.jpg"});
+            Students.Add(new Student() { Id = 2, Name = "Hassan", Address = "Aswan", Image = "2.jpg" });
+            Students.Add(new Student() { Id = 3, Name = "Hanan", Address = "Minia" , Image = "3.jpg" });
+            
+
+        }
+    }
+}
